95672ad7e10fb279bdc0cada9a7a8a28a271dc13
Raghu <raghu@Raghus-MacBook-Pro.local>
2022-12-22 08:38:39 +0530

    add-travis-file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState } from 'react';
+import {  useState } from 'react';
 
 function App() {
 
   const [count, setCount] = useState(0);
-
-  useEffect(()=>{
-    alert('side effect');
-    //return () => alert('good bye')
-  })
 
   return (
     <button onClick={() => setCount(count + 1)}>
